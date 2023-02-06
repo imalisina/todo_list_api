a006f29e3aff44bc96282fe68a8a3e8ec7464b99
Alisina Barghandan <barghandanalisina@gmail.com>
2023-02-07 00:23:52 +0530

    Finalize welcome.blade.php file
    - Changing UI
    - Adding link for GitHub Repo
    - Adding link for JSON data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,11 @@
             Use this API with your Front End !
             You can also use my ReactJs To-do App which uses this API.
         </p>
-        <a href="" type="button" class="mt-5 text-white bg-gradient-to-r from-pink-600 to-red-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-red-400 dark:focus:ring-pink-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-            Check ReactJs Todo App
+        <a href="https://github.com/imalisina?tab=repositories" type="button" class="mt-5 text-white bg-gradient-to-r from-pink-600 to-red-400 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-red-400 dark:focus:ring-pink-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                Check ReactJs Todo App
+        </a>
+        <a href="/api/tasks" type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+            Check JSON Data 
         </a>
     </div>
 </body>
