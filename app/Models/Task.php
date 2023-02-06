8365d516978db67019eaf23603f12111efff05a2
Alisina Barghandan <barghandanalisina@gmail.com>
2023-02-06 22:08:11 +0530

    Basic operations - Completed
    - Adding Factory & Seeder
    - Building Model & Controller
    - Define resource route for Tasks
    - Test in Postman

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    // Define fillable fields to prevent mass assignment error
+    protected $fillable = [
+        "title",
+        "description",
+        "status",
+        "timestamps",
+    ];
+    use HasFactory;
+}
